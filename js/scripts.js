055c3390295256548744186c8a83600dc2378d25
trevor ngugi <ngugitrevor@gmail.com>
2019-11-04 14:38:21 +0300

    pointer style in css

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $(".clickable1").click(function() {
+      $(".design-showing").toggle();
+      $(".design-hidden").toggle();
+    });
+  });
+  $(document).ready(function() {
+    $(".clickable2").click(function() {
+      $(".development-hidden").toggle();
+      $(".development-showing").toggle();
+    });
+  });
+  $(document).ready(function() {
+    $(".clickable3").click(function() {
+      $(".product-hidden").toggle();
+      $(".product-showing").toggle();
+    });
+  });
+
+
